ca1a13883e454aea1970b81a8ea40ba0a9548269
Adrien Thebo <git@somethingsinistral.net>
2017-05-08 13:46:28 -0700

    vte: only source file when present

--- home/.zshrc.d/vte.zsh
+++ home/.zshrc.d/vte.zsh
@@ -1,4 +1,6 @@
 # Source the VTE profile, so that when gnome-terminal opens a new terminal it
 # opens that terminal in the same directory.
 
-source /etc/profile.d/vte.sh
+if [ -f /etc/profile.d/vte.sh ]; then
+  source /etc/profile.d/vte.sh
+fi
