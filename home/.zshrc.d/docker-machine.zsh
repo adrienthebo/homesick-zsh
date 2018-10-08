8be54b3f991753959928e7117c0394cee8644bc8
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-08 09:21:59 -0700

    Add docker-machine helper

---
+++ home/.zshrc.d/docker-machine.zsh
@@ -0,0 +1,11 @@
+
+if [[ $(uname) = 'Darwin' ]]; then
+  function docker() {
+    command docker info 2>&- 1>&-
+    if [[ $? -eq 1 ]]; then
+      echo "-- Loading docker configuration - this may take a few seconds..."
+      eval $(docker-machine env default)
+    fi
+    command docker $@
+  }
+fi
