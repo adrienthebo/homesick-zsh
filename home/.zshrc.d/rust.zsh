a09865a86d5a419d2cbb90362e77ce243b1361c3
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-08 09:11:42 -0700

    Evaluate rustup completions when available

--- home/.zshrc.d/rust.zsh
+++ home/.zshrc.d/rust.zsh
@@ -3,3 +3,7 @@ pathmunge "${HOME}/.cargo/bin" after
 if command -v sccache 2>&- 1>&-; then
   export RUSTC_WRAPPER="sccache"
 fi
+
+if command -v rustup 2>&- 1>&-; then
+  eval $(rustup completions zsh)
+fi
