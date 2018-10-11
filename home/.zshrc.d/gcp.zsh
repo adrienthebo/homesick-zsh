eaac9e36d003744f06f6f5931c847e02da4a6a6d
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-10 21:20:55 -0700

    Typofix

--- home/.zshrc.d/gcp.zsh
+++ home/.zshrc.d/gcp.zsh
@@ -1,7 +1,7 @@
-if [ -f /opt/google-cloud-sdk/completion.zsh.inc ]; then
+if [[ -f /opt/google-cloud-sdk/completion.zsh.inc ]]; then
   source /opt/google-cloud-sdk/completion.zsh.inc
 fi
 
-if [ -f/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc ]; then
+if [[ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc ]]; then
   source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
