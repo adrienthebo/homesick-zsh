f99c04a15c58afaf58bb648b62d6bc4a12ff4bdf
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-27 16:55:11 -0700

    gcp: add gorg alias

--- home/.zshrc.d/gcp.zsh
+++ home/.zshrc.d/gcp.zsh
@@ -5,3 +5,7 @@ fi
 if [[ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc ]]; then
   source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
+
+function gorg() {
+  gcloud organizations list --format='value(name)'
+}
