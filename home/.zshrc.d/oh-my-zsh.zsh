207a58335d764cfaa906d80583a7887a9bb842bc
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-27 16:55:24 -0700

    powerlevel9: tweak prompt

--- home/.zshrc.d/oh-my-zsh.zsh
+++ home/.zshrc.d/oh-my-zsh.zsh
@@ -5,8 +5,12 @@ ZSH_CUSTOM="$HOME/.oh-my-zsh.custom"
 ZSH_THEME="powerlevel9k/powerlevel9k"
 #zsh_theme="fox"
 
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs time vi_mode)
+
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
+POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
 
 if [[ -f ~/.oh-my-zsh/oh-my-zsh.sh ]]; then
   source ~/.oh-my-zsh/oh-my-zsh.sh
