336cd2c2b105887f7b6314e690fb2d7b50d96812
Adrien Thebo <adrien@lagrange-automation.io>
2018-11-08 08:31:18 -0800

    Prefer python 2.7 over 3

--- home/.zshrc.d/python.zsh
+++ home/.zshrc.d/python.zsh
@@ -6,6 +6,6 @@ elif which pyenv 1>&- 2>&-; then
   eval "$(pyenv init -)"
 fi
 
-if [[ -d "/Users/thebo/Library/Python/3.7/bin" ]]; then
-  pathmunge "/Users/thebo/Library/Python/3.7/bin"
+if [[ -d "/Users/thebo/Library/Python/2.7/bin" ]]; then
+  pathmunge "/Users/thebo/Library/Python/2.7/bin"
 fi
