594c0a47a215699dad1bb52406c379672e785de7
Adrien Thebo <adrien@lagrange-automation.io>
2019-02-24 11:17:00 -0800

    Add more zsh completions, tweak powerlevel formatting

--- home/.zshrc.d/oh-my-zsh.zsh
+++ home/.zshrc.d/oh-my-zsh.zsh
@@ -8,21 +8,30 @@ plugins=(
   zsh-syntax-highlighting
   kitchen
   pip
-  history
-  fzf
+  history fzf
+  rustup
+  zsh-completions
 )
+
+autoload -U compinit && compinit
 
 ZSH="$HOME/.oh-my-zsh"
 ZSH_CUSTOM="$HOME/.oh-my-zsh.custom"
 ZSH_THEME="powerlevel9k/powerlevel9k"
 #zsh_theme="fox"
 
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode dir)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(vi_mode dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status background_jobs time)
 
 POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
 #POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+
+POWERLEVEL9K_VCS_GIT_HOOKS=()
+POWERLEVEL9K_VCS_SHOW_SUBMODULE_DIRTY=false
+POWERLEVEL9K_VCS_SHORTEN_LENGTH=24
+POWERLEVEL9K_VCS_SHORTEN_LENGTH=12
+POWERLEVEL9K_VCS_SHORTEN_STRATEGY="truncate_middle"
 
 if [[ -f ~/.oh-my-zsh/oh-my-zsh.sh ]]; then
   source ~/.oh-my-zsh/oh-my-zsh.sh
