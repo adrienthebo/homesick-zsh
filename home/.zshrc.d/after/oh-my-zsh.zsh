791365219b27d54e26727695a29824ffd118dfad
Adrien Thebo <adrien@lagrange-automation.io>
2019-04-08 22:03:44 -0700

    Improve lsd aliases

--- home/.zshrc.d/after/oh-my-zsh.zsh
+++ home/.zshrc.d/after/oh-my-zsh.zsh
@@ -1,4 +1,6 @@
 if command -v lsd 2>&- 1>&-; then
+  alias l="lsd"
+  alias ls="lsd -l"
   # oh-my-zsh has an `alias la="ls -lAh" which doesn't work with lsd.
-  alias la="ls -la"
+  alias la="lsd -la"
 fi
