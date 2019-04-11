40c684122521377661d2903d53c690bdb008696c
Adrien Thebo <adrien@lagrange-automation.io>
2019-04-10 18:56:47 -0700

    docker: don't add preexec hook without docker-machine

--- home/.zshrc.d/docker-machine.zsh
+++ home/.zshrc.d/docker-machine.zsh
@@ -1,10 +1,13 @@
-autoload -Uz add-zsh-hook
 
-preexec_dockermachine() {
-  if [[ $3 =~ "docker" ]] && [[ -z "$DOCKER_HOST" ]]; then
-    echo "-- preexec: configuring docker, this may take a few seconds..."
-    eval "$(docker-machine env default)"
-  fi
-}
+if command -v docker-machine; then
+  autoload -Uz add-zsh-hook
 
-add-zsh-hook preexec preexec_dockermachine
+  preexec_dockermachine() {
+    if [[ $3 =~ "docker" ]] && [[ -z "$DOCKER_HOST" ]]; then
+      echo "-- preexec: configuring docker, this may take a few seconds..."
+      eval "$(docker-machine env default)"
+    fi
+  }
+
+  add-zsh-hook preexec preexec_dockermachine
+fi
