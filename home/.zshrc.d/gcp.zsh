e313260d1297a9534e268a9ce174a67bb953be19
Adrien Thebo <adrien@lagrange-automation.io>
2020-08-13 10:37:24 -0700

    gcp: sdk support for mac

--- home/.zshrc.d/gcp.zsh
+++ home/.zshrc.d/gcp.zsh
@@ -1,1 +1,5 @@
 export CLOUDSDK_PYTHON=/usr/bin/python3
+
+if [[ -f "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]]; then
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
