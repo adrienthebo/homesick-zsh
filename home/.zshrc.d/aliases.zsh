4c26c5faa0b3033ed4291460278f49ac8dd7993c
Adrien Thebo <athebo@twitter.com>
2021-05-04 16:52:44 -0700

    Why yes, I am fucking hysterical ❇

--- home/.zshrc.d/aliases.zsh
+++ home/.zshrc.d/aliases.zsh
@@ -1,12 +1,9 @@
-alias y="echo n"
-
 alias qotd="nc alpha.mike-r.com 17"
 alias starwars="nc towel.blinkenlights.nl 23"
 alias tree="tree -C"
 
 alias terrorfarm="terraform"
 alias tf="terraform"
-alias t="task"
 
 if command -v colordiff 2>&- 1>&-; then
   alias diff="colordiff"
@@ -37,5 +34,29 @@ function cht() {
   curl https://cht.sh/$1 | $PAGER
 }
 
-alias ":q"="echo \"Hey kid, I'm a terminal! Stop all the vimming!\""
+# Laugh more, nitpick less. Y'all need to have more fun.
+alias ":q"="echo \"Hey kid, I'm a computer! Stop all the downloadin'!\""
 alias ":e"=":q"
+alias "ty"="yw!"
+alias "y"="echo n"
+
+if command -v gsed 2>&- 1>&-; then
+  git() {
+    if [[ $(($RANDOM % 64)) -eq 0 ]]; then
+      gti "$@"
+    else
+      command git "$@"
+    fi
+  }
+fi
+
+if command -v task 2>&- 1>&-; then
+  task() {
+    if [[ $(($RANDOM % 64)) -eq 0 ]]; then
+      echo "Go for a walk you banana shaped fuck."
+      sleep 60
+    fi
+    command task "$@"
+  }
+fi
+alias t="task"
