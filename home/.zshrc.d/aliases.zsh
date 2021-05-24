91b13620c6847838c535af7063676f90e62a982d
Adrien Thebo <athebo@twitter.com>
2021-05-24 12:01:03 -0700

    Improve command aliasing, replace ytop with bottom

--- home/.zshrc.d/aliases.zsh
+++ home/.zshrc.d/aliases.zsh
@@ -5,30 +5,23 @@ alias tree="tree -C"
 alias terrorfarm="terraform"
 alias tf="terraform"
 
-if command -v colordiff 2>&- 1>&-; then
-  alias diff="colordiff"
-fi
+maybe_alias() {
+  local from_cmd to_cmd
+  to_cmd="$1"
+  from_cmd="$2"
 
-if command -v nvim 2>&- 1>&-; then
-  alias vim=nvim
-fi
+  if command -v "$to_cmd" 2>&- 1>&-; then
+    alias $from_cmd="$to_cmd"
+  fi
+}
 
-if command -v lsd 2>&- 1>&-; then
-  alias ls=lsd
-fi
 
-if command -v bat 2>&- 1>&-; then
-  alias cat=bat
-  alias less=bat
-fi
-
-if command -v ytop 2>&- 1>&-; then
-  alias htop=ytop
-fi
-
-if command -v gsed 2>&- 1>&-; then
-  alias sed=gsed
-fi
+maybe_alias colordiff diff
+maybe_alias nvim vim
+maybe_alias lsd ls
+maybe_alias bat cat
+maybe_alias btm htop
+maybe_alias gsed sed
 
 function cht() {
   curl https://cht.sh/$1 | $PAGER
