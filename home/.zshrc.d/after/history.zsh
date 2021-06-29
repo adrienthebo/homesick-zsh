dd59b86c325c832d88ca8877e32b5c33118a527b
Adrien Thebo <athebo@twitter.com>
2021-06-29 09:59:16 -0700

    Move zsh history settings to before section

--- home/.zshrc.d/after/history.zsh
+++
@@ -1,8 +0,0 @@
-# Most of the history configuration is offloaded to oh-my-zh
-#
-# See also: https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/history.zsh
-
-HISTFILE=~/.histfile
-HIST_STAMPS="yyyy-mm-dd"
-HISTSIZE=1000000
-SAVEHIST=1000000
